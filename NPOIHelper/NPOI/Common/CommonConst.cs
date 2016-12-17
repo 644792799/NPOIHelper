7faf785d4d5bbf831414f9a6db550eb2274ffdf4
644792799@qq.com <644792799@qq.com>
2016-12-17 17:14:06 +0800

    样式添加简单化

---
+++ NPOIHelper/NPOI/Common/CommonConst.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPOIHelper.NPOI.Common
+{
+    public class CommonConst
+    {
+        public const string[] STANDARD_STYLE_PROPERTIES = 
+        { 
+            "border-top", 
+            "border-right", 
+            "border-bottom", 
+            "border-left" ,
+            "border-top-color",
+            "border-right-color",
+            "border-bottom-color",
+            "border-left-color",
+            "background-color",
+            "border-diagonal",
+            "border-diagonal-color",
+            "border-diagonal-lines-tyle",
+            "color",
+            "data-format",
+            "vertical-align",
+            "text-align"
+        };
+    }
+}
---
+++ NPOIHelper/NPOI/Excel/ExcelCellStyle.cs
@@ -0,0 +1,113 @@
+﻿using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPOIHelper.NPOI.Excel
+{
+    public static class ExcelCellStyle
+    {
+        public static void Style(this ICell cell, string style)
+        {
+            Dictionary<string, string> dicproperties = style.ToDic();
+            IWorkbook workbook = cell.Sheet.Workbook;
+            ICellStyle cellstyle;
+            if (cell.CellStyle == null)
+            {
+                cellstyle = workbook.CreateCellStyle();
+            }
+            else
+            {
+                cellstyle = workbook.CreateCellStyle();
+                cellstyle.CloneStyleFrom(cell.CellStyle);
+            }
+            cell.CellStyle = cellstyle;
+        }
+
+        public static Dictionary<string, string> ToDic(this string style)
+        {
+            Dictionary<string, string> dicproperties = new Dictionary<string, string>();
+            if (style.EndsWith(";"))
+            {
+                style = style.TrimEnd(new char[] { ';' });
+            }
+            string[] propertiesArr = style.Split(new char[] { ';' });
+            foreach (var p in propertiesArr)
+            {
+                string[] kv = p.Split(new char[] { ':' });
+                string key = kv[0].Trim();
+                string value = kv[1].Trim();
+                if (!dicproperties.ContainsKey(key) && Common.CommonConst.STANDARD_STYLE_PROPERTIES.Contains(key))
+                {
+                    dicproperties.Add(key, value);
+                }
+            }
+            return dicproperties;
+        }
+
+        public static void AttachStyleToCellStyle(ICellStyle cellstyle, Dictionary<string, string> prop)
+        {
+            foreach (var p in prop)
+            {
+                cellstyle.AttachProperty(p.Key, p.Value);
+            }
+        }
+
+        public static void AttachProperty(this ICellStyle cellstyle, string key, string value)
+        {
+            switch (key)
+            {
+                case "border-top":
+                    cellstyle.BorderTop = (BorderStyle)Enum.ToObject(typeof(BorderStyle), value);
+                    break;
+                case "border-right":
+                    cellstyle.BorderRight = (BorderStyle)Enum.ToObject(typeof(BorderStyle), value);
+                    break;
+                case "border-bottom":
+                    cellstyle.BorderBottom = (BorderStyle)Enum.ToObject(typeof(BorderStyle), value);
+                    break;
+                case "border-left":
+                    cellstyle.BorderLeft = (BorderStyle)Enum.ToObject(typeof(BorderStyle), value);
+                    break;
+                case "border-top-color":
+
+                    break;
+                case "border-right-color":
+
+                    break;
+                case "border-bottom-color":
+
+                    break;
+                case "border-left-color":
+
+                    break;
+                case "background-color":
+
+                    break;
+                case "border-diagonal":
+
+                    break;
+                case "border-diagonal-color":
+
+                    break;
+                case "border-diagonal-lines-tyle":
+
+                    break;
+                case "color":
+
+                    break;
+                case "data-format":
+
+                    break;
+                case "vertical-align":
+
+                    break;
+                case "text-align":
+
+                    break;
+            }
+        }
+    }
+}
